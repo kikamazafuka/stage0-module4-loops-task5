9eb00a0cac716d67128d80ceb71a96568925e0d8
Artur Semenas <uncelfuka@gmail.com>
2023-11-12 15:33:54 +0100

    Module #4.2. Task #5. Nested Loops

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,9 +1,7 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public static void main(String[] args) {
-        printHourglassOfGivenSize(5);
-    }
+
     public static void printHourglassOfGivenSize(int height) {
         for (int i = 0; i<height; i++){
             for (int j = 0; j<height; j++){
@@ -16,16 +14,6 @@ public class Hourglass {
             }
             System.out.println("");
         }
-//        for (int i = 0; i < height; i++) {
-//            for (int j = 0; j < height; j++) {
-//                if (i == 0 || i == height - 1 || j >= i && j < height - i - 1 || i >= height / 2 && j <= height + height / 2 - i - 1) {
-//                    System.out.print("8");
-//                } else {
-//                    System.out.print(" ");
-//                }
-//            }
-//            System.out.println();
-//        }
     }
 
 
--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
         for (int i = 0; i<height; i++){
             for (int j = 0; j<length; j++){
                 if(i==0 &&j!=length-1){
