83cf17ae691f863e7939776cda130aaa8a4fe4c1
Artur Semenas <uncelfuka@gmail.com>
2023-11-12 15:31:33 +0100

    Module #4.2. Task #5. Nested Loops

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,32 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
-
+    public static void main(String[] args) {
+        printHourglassOfGivenSize(5);
     }
+    public static void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i<height; i++){
+            for (int j = 0; j<height; j++){
+                if (i==0 || i==height-1 || (j>=i && j<height-i)){
+                    System.out.print(8);
+                } else if (i>height/2 && j>0) {
+                    System.out.print(8);
+                } else System.out.print(" ");
+
+            }
+            System.out.println("");
+        }
+//        for (int i = 0; i < height; i++) {
+//            for (int j = 0; j < height; j++) {
+//                if (i == 0 || i == height - 1 || j >= i && j < height - i - 1 || i >= height / 2 && j <= height + height / 2 - i - 1) {
+//                    System.out.print("8");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+    }
+
+
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,13 @@
 package school.mjc.stage0.loops.task5;
 
 public class Triangle {
-    public void printTriangle(int cathetusLength) {
 
+    public void printTriangle(int cathetusLength) {
+        for (int i = 0; i<cathetusLength; i++){
+            for (int j = 0; j<i+1; j++){
+                System.out.print(8);
+            }
+            System.out.println("");
+        }
     }
 }
